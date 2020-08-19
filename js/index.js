258e3442c3560eafa0414b5b8b9763c9a6808575
Jabo <joelvincent.work@gmail.com>
2020-08-20 00:04:17 +0800

    🔧 Added adjustmen/fixt where pages returns 404

--- js/index.js
+++ js/index.js
@@ -5,12 +5,19 @@ links.forEach(link => {
     link.addEventListener('click', e => {
         e.preventDefault();
         let target = e.target.getAttribute("href").replace(".html", "");
-        window.history.pushState({}, target, window.location.origin + target);
+        window.history.pushState({}, target, window.location.origin + "#" + target.replace("/", ""));
         document.dispatchEvent(urlChange);
     })
 })
 
 document.addEventListener("url-update", async e => {
-    let page = await fetch(location.pathname + ".html")
+    updatePage();
+});
+
+updatePage();
+
+async function updatePage() {
+    let page = await fetch((location.hash.replace("#", "") || 'home') + ".html")
     root.innerHTML =await page.text();
-})
+}
+
